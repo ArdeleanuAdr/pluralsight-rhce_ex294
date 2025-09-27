cc8f7714bf43df1d6d3dd7e7db9613060cf5af46
ArdeleanuAdr <roa.adrian@yahoo.com>
2025-09-27 13:54:24 +0300

    A python templating script for String manipulation but doesn';t work in this version

--- linuxist/my_collection/plugins/filter/upper.py
+++ linuxist/my_collection/plugins/filter/upper.py
@@ -1,13 +1,13 @@
-from ansible.errors import AnsibleFilterError
-
-def to_upper(value str):
-    if not isinstance(value, str):
-        raise AnsibleFilterError("The provided value must be a string")
-    return value.upper()
-
-class FilterModule:
-    def filters(self):
-        return {
-                'upper_case': to_upper,
-                }
-
+#from ansible.errors import AnsibleFilterError
+#
+#def to_upper(value):
+#    if not isinstance(value, str):
+#        raise AnsibleFilterError("The provided value must be a string")
+#    return value.upper()
+#
+#class FilterModule(object):
+#    def filters(self):
+#        return {
+#            "upper_case": to_upper,
+#        }
+#
